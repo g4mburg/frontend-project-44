5368f2aa1ed5414b3156686c853f28a140892f39
Андрей <ulsk0073@gmail.com>
2023-02-07 17:48:21 +0400

    fix src/cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,10 @@
 import readlineSync from 'readline-sync';
 
 const nameQuestion = () => {
-  const name = readlineSync.question('May I have your name?');
+  const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
 
   console.log(`Hello, ${name}!`);
+  return name;
 };
 
 export default nameQuestion;
