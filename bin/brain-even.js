de5dece8986dcb9a760c90c33d43d93254f2b00d
Андрей <ulsk0073@gmail.com>
2023-02-24 19:23:41 +0400

    fix brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -2,8 +2,7 @@
 import nameQuestion from '../src/cli.js';
 import logicOfGames from '../src/index.js';
 
-function evenGame() {
-  const name = nameQuestion();
+function evenGame(name) {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
   for (let i = 0; i < 3; i += 1) {
     const number = Math.ceil(Math.random() * 100);
@@ -20,4 +19,5 @@ function evenGame() {
     }
   }
 }
-evenGame();
+const name = nameQuestion();
+evenGame(name);
