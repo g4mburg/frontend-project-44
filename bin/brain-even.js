2c13a6ce02eb81672b9cb79362792617662b4f94
Андрей <ulsk0073@gmail.com>
2023-02-26 13:56:42 +0400

    mod brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,23 +1,4 @@
 #!/usr/bin/env node
-import nameQuestion from '../src/cli.js';
-import logicOfGames from '../src/index.js';
+import evenGame from '../src/games/even-game.js';
 
-function evenGame(name) {
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const number = Math.ceil(Math.random() * 100);
-    let correctAnswer = '';
-    if (number % 2 === 0) {
-      correctAnswer = 'yes';
-    } else {
-      correctAnswer = 'no';
-    }
-    const correctChecker = logicOfGames(i, number, correctAnswer, name);
-    console.log(correctChecker);
-    if (correctChecker !== 'Correct!') {
-      break;
-    }
-  }
-}
-const name = nameQuestion();
-evenGame(name);
+evenGame();
---
+++ src/games/even-game.js
@@ -0,0 +1,21 @@
+import nameQuestion from '../cli.js';
+import logicOfGames from '../index.js';
+
+export default function evenGame() {
+  const name = nameQuestion();
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const number = Math.ceil(Math.random() * 100);
+    let correctAnswer = '';
+    if (number % 2 === 0) {
+      correctAnswer = 'yes';
+    } else {
+      correctAnswer = 'no';
+    }
+    const correctChecker = logicOfGames(i, number, correctAnswer, name);
+    console.log(correctChecker);
+    if (correctChecker !== 'Correct!') {
+      break;
+    }
+  }
+}
