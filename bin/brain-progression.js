255fde5ca29ef1b7d1d31ae3d43931a034a0deff
Андрей <ulsk0073@gmail.com>
2023-02-26 14:33:22 +0400

    mod brain-progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,33 +1,4 @@
 #!/usr/bin/env node
-import nameQuestion from '../src/cli.js';
-import logicOfGames from '../src/index.js';
+import brainProgression from '../src/games/progression-game.js';
 
-function brainProgression() {
-  const name = nameQuestion();
-  console.log('What number is missing in the progression?');
-  function getRandomIntInclusive(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  for (let i = 0; i < 3; i += 1) {
-    let number = '';
-    let correctAnswer = 0;
-    const step = getRandomIntInclusive(2, 7);
-    const numberOfHiddenNumber = getRandomIntInclusive(0, 9);
-    let currentNumber = getRandomIntInclusive(1, 50);
-    for (let j = 0; j < 10; j += 1) {
-      if (j !== numberOfHiddenNumber) {
-        number = (`${number} ${currentNumber}`);
-      } else {
-        correctAnswer = currentNumber;
-        number = (`${number} ..`);
-      }
-      currentNumber += step;
-    }
-    const correctChecker = logicOfGames(i, number, correctAnswer, name);
-    console.log(correctChecker);
-    if (correctChecker !== 'Correct!') {
-      break;
-    }
-  }
-}
 brainProgression();
---
+++ src/games/progression-game.js
@@ -0,0 +1,31 @@
+import nameQuestion from '../cli.js';
+import logicOfGames from '../index.js';
+
+export default function brainProgression() {
+  const name = nameQuestion();
+  console.log('What number is missing in the progression?');
+  function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  for (let i = 0; i < 3; i += 1) {
+    let number = '';
+    let correctAnswer = 0;
+    const step = getRandomIntInclusive(2, 7);
+    const numberOfHiddenNumber = getRandomIntInclusive(0, 9);
+    let currentNumber = getRandomIntInclusive(1, 50);
+    for (let j = 0; j < 10; j += 1) {
+      if (j !== numberOfHiddenNumber) {
+        number = (`${number} ${currentNumber}`);
+      } else {
+        correctAnswer = currentNumber;
+        number = (`${number} ..`);
+      }
+      currentNumber += step;
+    }
+    const correctChecker = logicOfGames(i, number, correctAnswer, name);
+    console.log(correctChecker);
+    if (correctChecker !== 'Correct!') {
+      break;
+    }
+  }
+}
