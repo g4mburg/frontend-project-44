2b908d38dac57c61bff4961edda808612c18f8cc
Андрей <ulsk0073@gmail.com>
2023-02-26 14:29:53 +0400

    mod brain-prime

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,29 +1,4 @@
 #!/usr/bin/env node
-import nameQuestion from '../src/cli.js';
-import logicOfGames from '../src/index.js';
+import primeGame from '../src/games/prime-game.js';
 
-function primeGame() {
-  const name = nameQuestion();
-  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-  for (let i = 0; i < 3; i += 1) {
-    const number = Math.ceil(Math.random() * 100);
-    let correctAnswer = '';
-    const limit = Math.sqrt(number);
-    let j = 1;
-    while (j <= limit) {
-      j += 1;
-      if (number % j === 0) {
-        correctAnswer = 'no';
-        break;
-      } else {
-        correctAnswer = 'yes';
-      }
-    }
-    const correctChecker = logicOfGames(i, number, correctAnswer, name);
-    console.log(correctChecker);
-    if (correctChecker !== 'Correct!') {
-      break;
-    }
-  }
-}
 primeGame();
---
+++ src/games/prime-game.js
@@ -0,0 +1,27 @@
+import nameQuestion from '../cli.js';
+import logicOfGames from '../index.js';
+
+export default function primeGame() {
+  const name = nameQuestion();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const number = Math.ceil(Math.random() * 100);
+    let correctAnswer = '';
+    const limit = Math.sqrt(number);
+    let j = 1;
+    while (j <= limit) {
+      j += 1;
+      if (number % j === 0) {
+        correctAnswer = 'no';
+        break;
+      } else {
+        correctAnswer = 'yes';
+      }
+    }
+    const correctChecker = logicOfGames(i, number, correctAnswer, name);
+    console.log(correctChecker);
+    if (correctChecker !== 'Correct!') {
+      break;
+    }
+  }
+}
